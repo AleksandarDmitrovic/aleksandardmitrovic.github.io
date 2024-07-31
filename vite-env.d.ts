955aa81dc35616c5ccef08b471c5d6403defbc49
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2024-07-31 17:01:14 -0600

    Test adding env secret for linking to externally hosted asset

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_EMAILJS_SERVICE_ID: string;
   readonly VITE_APP_EMAILJS_TEMPLATE_ID: string;
   readonly VITE_APP_EMAILJS_PUBLIC_KEY: string;
+  readonly S3_GLB_URL: string;
   // more env variables...
 }
 
