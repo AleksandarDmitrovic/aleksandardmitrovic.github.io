b3ffb6d61a79ed3d3e2e09b33762f637d2196a57
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2024-07-31 17:18:29 -0600

    Fix styling of loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <Html>
       <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+        <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600"></div>
       </div>
     </Html>
   );
