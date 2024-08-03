35bb436435b0018d01daaf5c3ff73c9a54d7d0df
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2024-08-02 22:32:07 -0600

    Add smaller island mountain glb file

--- src/models/Island.tsx
+++ src/models/Island.tsx
@@ -3,11 +3,13 @@ import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { a } from "@react-spring/three";
 
+import mountainIslandScene from "../assets/3d/island_mountain.glb";
+
 const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
   const islandRef = useRef();
   // Get access to the Three.js renderer and viewport
   const { gl, viewport } = useThree();
-  const { nodes, materials } = useGLTF(import.meta.env.VITE_APP_S3_GLB_URL);
+  const { nodes, materials } = useGLTF(mountainIslandScene);
 
   // Use a ref for the last mouse x position
   const lastX = useRef(0);
